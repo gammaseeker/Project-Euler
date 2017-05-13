67cfb94eee7f7dbc582e915d745229d6fee9dce3
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-05-13 15:13:23 -0400

    Add files via upload

---
+++ Python Project Euler/Euler6.py
@@ -0,0 +1,8 @@
+#Joey Jiemjitpolchai
+sumSquares = 0
+squareOfSum = 0
+N = 100
+sumSquares = N * (N+1)/2
+squareOfSum = (N * (N + 1)  * (2 * N + 1))/6
+output = sumSquares * sumSquares - squareOfSum
+print(output)
